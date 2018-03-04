5f7881b9b50fc29172e5cc016f14c20eb11127ce
zhengli <lizheng808@gmail.com>
2018-03-04 14:33:31 -0800

    merge

--- logreg.py
+++ logreg.py
@@ -6,15 +6,18 @@ import numpy as np
 X = pd.read_csv('data.csv', encoding = "utf8")
 print(X.shape)
 X.dropna(how='all', inplace = True)
-#X = X[np.notnull(X)]
-
 
 X = X.reset_index(drop=True)
 
 print(X.head())
 
 
+X = X.data[:,:,2]
+print(X.head())
+
 # all parameters not specified are set to their defaults
 logisticRegr = LogisticRegression()
+
+
 
 
